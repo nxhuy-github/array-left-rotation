20a09040cc885315a4ccff1c516481f0b02a557c
Huy <xuan-huy.nguyen@etu.univ-lyon1.fr>
2019-10-27 20:25:19 +0100

    change index of args

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from LeftRotation import LeftRotation
 
 if __name__ == "__main__":
-    string = sys.argv[0]
-    nb_rotation = sys.argv[1]
+    string = sys.argv[1]
+    nb_rotation = int(sys.argv[2])
     left_rotation = LeftRotation()
     print(left_rotation.rotate(string, nb_rotation))
