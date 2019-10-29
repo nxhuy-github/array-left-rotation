4226acdc67ea39a7664e748d8cc018cdc27999b4
Huy <xuan-huy.nguyen@etu.univ-lyon1.fr>
2019-10-29 09:12:25 +0100

    update

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from LeftRotation import LeftRotation
 
 if __name__ == "__main__":
     string = sys.argv[1]
-    nb_rotation = int(sys.argv[2])
-    left_rotation = LeftRotation()
-    print(left_rotation.rotate(string, nb_rotation))
+    try:
+        nb_rotation = int(sys.argv[2])
+        left_rotation = LeftRotation()
+        print(left_rotation.rotate(string, nb_rotation))
+    except ValueError:
+        print('The second argument must be an integer...')
